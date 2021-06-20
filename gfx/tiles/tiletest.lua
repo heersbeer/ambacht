d5cd523a6216e088dfd3382e2e0b1d41e03ae845
Marko <markovang@gmail.com>
2021-06-20 21:05:32 +0200

    Eerste werkende versie met tiles

---
+++ gfx/tiles/tiletest.lua
@@ -0,0 +1,71 @@
+return {
+  version = "1.5",
+  luaversion = "5.1",
+  tiledversion = "1.7.0",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 8,
+  height = 8,
+  tilewidth = 32,
+  tileheight = 32,
+  nextlayerid = 2,
+  nextobjectid = 1,
+  properties = {},
+  tilesets = {
+    {
+      name = "32x32_map_tile v3.1",
+      firstgid = 1,
+      tilewidth = 32,
+      tileheight = 32,
+      spacing = 1,
+      margin = 0,
+      columns = 23,
+      image = "gfx/tiles/32x32_map_tile v3.1.png",
+      imagewidth = 760,
+      imageheight = 958,
+      objectalignment = "unspecified",
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 32,
+        height = 32
+      },
+      properties = {},
+      wangsets = {},
+      tilecount = 667,
+      tiles = {}
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 8,
+      height = 8,
+      id = 1,
+      name = "Tile Layer 1",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        25, 25, 25, 25, 25, 25, 25, 25,
+        25, 25, 25, 27, 25, 25, 25, 25,
+        25, 25, 25, 27, 25, 25, 25, 25,
+        25, 25, 25, 27, 25, 25, 25, 25,
+        25, 25, 25, 27, 25, 25, 25, 25,
+        25, 25, 25, 25, 25, 25, 25, 25,
+        25, 25, 25, 25, 25, 25, 25, 25,
+        25, 25, 25, 25, 25, 25, 25, 25
+      }
+    }
+  }
+}
--- main.lua
+++ main.lua
@@ -1,20 +1,23 @@
 function love.load()
+
   love.window.setMode(1024, 768, {resizable=false})
 
   windfield = require('lib/windfield/')
   anim8 = require('lib/anim8/anim8')
   world = windfield.newWorld()
-
+  require('conf')
 
   require("src/collisionClasses")
   createCollisionClasses()
 
   require('src/resources')
   require('src/player')
+  require('src/tiledmap')
 
-  testBlock = world:newRectangleCollider(282, -90, 198, 198)
-  testBlock:setType('static')
+  map = loadTiledMap('gfx/tiles/tiletest')
 
+  --testBlock = world:newRectangleCollider(282, -90, 198, 198)
+  --testBlock:setType('static')
 end
 
 function love.update(dt)
@@ -23,8 +26,7 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.draw(textures.background)
-
+  map:draw()
   player:draw()
   world:draw()
 end
